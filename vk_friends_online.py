804424ee4da06f424d4998b0397ce231a301f057
Nogmanov Ildar <nogmanov@gmail.com>
2017-07-15 16:14:38 +0300

    Clone originalrepo. Replace after clone fork

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,29 +1,32 @@
+import getpass
 import vk
 
-
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
-
+APP_ID = 6101946
 
 def get_user_login():
-    pass
-
+    typed_login = input('Please type login: ')
+    return typed_login
 
 def get_user_password():
-    pass
-
+    typed_password = getpass.getpass(prompt='Please type password: ')
+    return typed_password
 
 def get_online_friends(login, password):
     session = vk.AuthSession(
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope='friends'
     )
     api = vk.API(session)
-    # например, api.friends.get()
-
+    friends_id_online = api.friends.getOnline()
+    friends_online = api.users.get(user_ids=friends_id_online)
+    return friends_online
 
 def output_friends_to_console(friends_online):
-    pass
+    print('My friends online: ')
+    for friend in friends_online:
+        print(friend['first_name'], friend['last_name'])
 
 if __name__ == '__main__':
     login = get_user_login()
